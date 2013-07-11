c12d3918874d3de8f00b4f7a5839e1addb2f9342
Rex Justin Lim <rexjustinlim@gmail.com>
2013-07-11 12:48:22 -0500

    ddominator custom with Google bootanimation and without Apollo and CMWallpapers

--- config/common.mk
+++ config/common.mk
@@ -154,8 +154,8 @@ PRODUCT_PACKAGES += \
     DSPManager \
     libcyanogen-dsp \
     audio_effects.conf \
-    CMWallpapers \
-    Apollo \
+#    CMWallpapers \
+#    Apollo \
     CMUpdater \
     CMFileManager \
     LockClock
@@ -224,7 +224,7 @@ ifdef CM_BUILDTYPE
     endif
 else
     # If CM_BUILDTYPE is not defined, set to UNOFFICIAL
-    CM_BUILDTYPE := UNOFFICIAL
+    CM_BUILDTYPE := ddominator
     CM_EXTRAVERSION :=
 endif
 
