082c39de00bbf55808373ef9bd07519614a6854d
Rex Justin Lim <me@rexjustinlim.com>
2013-07-15 09:16:21 -0500

    Clean up

--- config/common.mk
+++ config/common.mk
@@ -159,11 +159,7 @@ PRODUCT_PACKAGES += \
     DSPManager \
     libcyanogen-dsp \
     audio_effects.conf \
-#    CMWallpapers \
-#    Apollo \
-    CMUpdater \
-    CMFileManager \
-    LockClock
+    CMFileManager 
 
 PRODUCT_PACKAGES += \
     CellBroadcastReceiver
